089bc195dd900a642c34b391ae97c14dd7323971
seyfettin <tulumseyfettin1@gmail.com>
2025-06-11 00:42:21 +0300

    Update .dockerignore to exclude npm-debug.log and .git; change server port from 8080 to 3000 in index.ts

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,6 @@ app.use("/", authRouter);
 
 app.use(errorMiddleware);
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
